8615b2eef17165eee8c12fab8913585b712e7cf6
Khaltech99 <olatunjibabatundejunior@gmail.com>
2025-11-08 22:28:05 +0000

    ...index

---
+++ app/index.jsx
@@ -0,0 +1,11 @@
+import { View } from "react-native";
+import Homescreen from "../components/Homescreen";
+import "../global.css";
+
+export default function Index() {
+  return (
+    <View>
+      <Homescreen />
+    </View>
+  );
+}
