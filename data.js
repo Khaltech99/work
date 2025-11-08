689d0415bff75997a95aad4d38b06b6c34f2f733
Khaltech99 <olatunjibabatundejunior@gmail.com>
2025-11-08 22:26:44 +0000

    Add initial data structure and Tailwind CSS setup

---
+++ data.js
@@ -0,0 +1,5 @@
+export const data = [
+  { title: "Upcoming", color: "#FBC02D", count: 2, bgColor: "#FFF9C4" },
+  { title: "Inactive", color: "#5044EE", count: 1, bgColor: "#C5CAE9" },
+  { title: "Done", color: "#66BB6A", count: 1, bgColor: "#C8E6C9" },
+];
