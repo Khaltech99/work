2af954010a5a228726e1d7b822e1095827429f7a
Khaltech99 <olatunjibabatundejunior@gmail.com>
2025-11-09 21:37:08 +0000

    learnt how to manage network connection status in RN

--- app/plus.jsx
+++ app/plus.jsx
@@ -1,11 +1,33 @@
+import { addEventListener } from "@react-native-community/netinfo";
+import { useEffect, useState } from "react";
 import { Text, View } from "react-native";
 
-const Plus = () => {
+const Offline = () => {
   return (
     <View>
-      <Text>Plus</Text>
+      <Text>User is offline</Text>
     </View>
   );
+};
+
+const Online = () => {
+  return (
+    <View>
+      <Text>User is online</Text>
+    </View>
+  );
+};
+
+const Plus = () => {
+  const [isOnline, setIsOnline] = useState(false);
+  useEffect(() => {
+    const unsubscribe = addEventListener((state) => {
+      setIsOnline(state.isConnected);
+    });
+
+    return unsubscribe();
+  }, []);
+  return <View>{isOnline ? <Online /> : <Offline />}</View>;
 };
 
 export default Plus;
